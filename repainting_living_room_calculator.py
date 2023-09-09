1500f78693c9206fdad0759dc90a24892533bbdd
Elena Nikolova <125276832+elenanikolova896@users.noreply.github.com>
2023-09-09 11:16:53 +0300

    Update repainting_living_room_calculator.py

--- repainting_living_room_calculator.py
+++ repainting_living_room_calculator.py
@@ -13,15 +13,18 @@ paint_weakner = int(input("How many liters of paint weakner do you need?"))
 
 # Total sum of materials calculation
 nylon_sum = (nylon + 2) * nylon_for_sqmeter
-paint_sum = paint + (paint * 10/100) * paint_for_liter
+paint_sum = (paint + (paint * 0.1)) * paint_for_liter
 paint_weakner_sum = paint_weakner * paint_weakner_for_liter
 packages = 0.40
 total_sum_for_materials = nylon_sum + paint_sum + paint_weakner_sum + packages
 
-sum_for_workers_per_hour = total_sum_for_materials * (total_sum_for_materials * (30/100) )
+# Calculate cost for workers
 hours_of_work = int(input("How many hours will the workers need to pain the living room?"))
+sum_for_workers_per_hour = total_sum_for_materials * 0.3
 total_sum_for_workers = hours_of_work * sum_for_workers_per_hour
 
+# Calculate total cost
 total_amount_of_everything = total_sum_for_materials + total_sum_for_workers
 
-print(total_amount_of_everything)
+#Print the total cost
+print("Total cost: ", total_amount_of_everything)
